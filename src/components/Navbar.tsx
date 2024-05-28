323f779485e7e6916baff3cb0deb9e31958e3de8
lodrixoll <lodrixoll@gmail.com>
2024-05-27 17:27:54 -0700

    fix lint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     return (
         <nav className="w-full fixed top-10">
             <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-                <a className="text-2xl font-bold text-gray-900" href="/">LLM2LLC</a>
+                <Link href="/" passHref>
+                    <span className="text-2xl font-bold text-gray-900">LLM2LLC</span>
+                </Link>
                 <div className="space-x-4">
                     <Link href="/" passHref>
                         <span className="text-gray-700 hover:text-gray-900">Home</span>
