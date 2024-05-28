6f7a51df6c5e44dd03423509a407cee625a7306f
lodrixoll <lodrixoll@gmail.com>
2024-05-27 17:22:00 -0700

    fix text size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ const Home = () => {
             <Navbar />
             <div className="container mx-auto text-center flex-grow flex flex-col justify-center">
                 <h1 className="text-5xl font-bold text-gray-900 mb-4">LLM2LLC</h1>
-                <p className="text-xl text-gray-700 mb-6">AI solution for LLC formation and small business growth.</p>
+                <p className="text-2xl text-gray-700 my-4">AI solution for LLC formation and small business growth.</p>
                 <hr className="w-1/2 border-gray-300 my-4 mx-auto" />
-                <p className="text-lg text-gray-700 mb-8">Built for Founders, Consultants, & Entrepreneurs.</p>
+                <p className="text-2xl text-gray-700 mb-8">Built for Founders, Consultants, & Entrepreneurs.</p>
                 <div className="space-x-4 mb-8">
                     <Link href="/register" passHref>
                         <span className="bg-mint-500 text-white py-3 px-6 rounded-lg text-lg hover:bg-mint-600 transition duration-300" role="button">Get Started</span>
