e5a5a40f4a082d508fe3a0e3b1d401d576a87d91
lodrixoll <lodrixoll@gmail.com>
2024-05-27 18:05:29 -0700

    add session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <SessionProvider>
             <Head>
                 <title>llm2llc</title>
             </Head>
             <Component {...pageProps} />
-        </>
+        </SessionProvider>
     );
 }
 
